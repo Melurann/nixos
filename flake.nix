569363ba32ac49fdd83327db6a7f3a13c404773d
melurann <fruroa21@htl-kaindorf.at>
2024-11-26 20:08:07 +0100

    basic flake.nix setup

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.fruth = nixpkgs.lib.nixosSystem { 
+    system = "x86_64-linux"; 
+    modules = [./configuration.nix];
+    };
+  };
+}
---
+++ modules/git.nix
@@ -0,0 +1,7 @@
+{...}: {
+    programs.git = {
+        enable = true;
+        userName = "melurann";
+        userEmail = "fruthroman@gmail.com";
+    }
+}
