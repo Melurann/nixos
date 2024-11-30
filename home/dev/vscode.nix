c762532950e5db37fe44eaad5727ef6edfb60ee4
melurann <fruroa21@htl-kaindorf.at>
2024-11-30 17:17:42 +0100

    add userSettings for vscode

--- home/dev/vscode.nix
+++ home/dev/vscode.nix
@@ -1,6 +1,12 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
+
+    userSettings = {
+      "files.autoSave" = "onFocusChange";
+      "editor.formatOnSave" = true;
+      "workbench.colorTheme" = "Catppuccin Mocha";
+    };
     extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
       github.copilot
