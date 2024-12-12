b6f28c70087cd48433d22b1d246f91f835e98f01
melurann <fruroa21@htl-kaindorf.at>
2024-12-12 17:48:07 +0100

    fix waybar temperature

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -95,10 +95,11 @@
           on-click = "kitty -e btop";
         };
         "temperature" = {
+          hwmon-path = "/sys/class/hwmon/hwmon2/temp1_input";
           critical-threshold = 80;
           format-critical = " {temperatureC}°C";
           format = " {temperatureC}°C";
-          on-click = "kiity -e btop";
+          on-click = "kitty -e btop";
         };
         "backlight" = {
           format = "{icon} {percent}%";
