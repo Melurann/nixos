94b214e3e2f29ef02d8726ed282a435246a3fff3
melurann <fruroa21@htl-kaindorf.at>
2024-12-20 11:29:27 +0100

    add alias to open current repo in browser

--- home/git.nix
+++ home/git.nix
@@ -8,6 +8,9 @@
     userName = "melurann";
     userEmail = "fruroa21@htl-kaindorf.at";
     ignores = ["*~" ".DS_Store" ".direnv"];
+    aliases = {
+      open = "!sh -c 'url=$(git config --get remote.origin.url); if [[ $url == git@* ]]; then url=$(echo $url | sed -e \"s/:/\\//\" -e \"s/^git@/https:\\/\\//\" -e \"s/\\.git$//\"); fi; xdg-open $url'";
+    };
     extraConfig = {
       pull = {
         rebase = true;
