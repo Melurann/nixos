e19889eecc4764e33d05476418bbf3a0c1c8a882
melurann <fruroa21@htl-kaindorf.at>
2025-02-25 18:56:30 +0100

    change vscode tree indent

--- home/dev/vscode.nix
+++ home/dev/vscode.nix
@@ -7,6 +7,7 @@
       "editor.formatOnSave" = true;
       "workbench.colorTheme" = "Catppuccin Mocha";
       "workbench.editor.editorActionsLocation" = "hidden";
+      "workbench.tree.indent" = 20;
     };
     extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
