d90f49c030dc47866bfcf2d0623280ce3afbe488
melurann <fruroa21@htl-kaindorf.at>
2025-11-18 10:06:59 +0100

    feat(flake): add multi host compatibility & global user

--- flake.nix
+++ flake.nix
@@ -15,26 +15,81 @@
     };
   };
 
-  outputs = {
+  outputs = inputs @ {
     nixpkgs,
     home-manager,
     ...
-  } @ inputs: {
-    nixosConfigurations.fruroa = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.fruroa = import ./hosts/home.nix;
-            backupFileExtension = "backup";
-          };
-        }
-      ];
+  }: let
+    # ------------------------------------
+    # Global user
+    # ------------------------------------
+    user = {
+      name = "fruroa";
+      homeDir = "/home/fruroa";
     };
+
+    # ------------------------------------
+    # Hosts
+    # ------------------------------------
+    hosts = [
+      {
+        name = "fujitsu-laptop";
+        system = {
+          os = "linux";
+          desktop = "wayland";
+        };
+        monitors = [
+          {
+            name = "HDMI-A-1";
+            dimensions = "1920x1080";
+            scale = 1;
+            primary = true;
+            internal = true;
+            framerate = 60;
+            transform = 0;
+          }
+        ];
+      }
+    ];
+
+    # ------------------------------------
+    # build each host
+    # ------------------------------------
+    forLinuxHosts = host: {
+      name = host.name;
+      value = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit inputs;
+          meta = {
+            hostname = host.name;
+            system = host.system;
+            monitors = host.monitors;
+          };
+          user = user;
+        };
+
+        modules = [
+          ./hosts/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              backupFileExtension = "backup";
+              users.${user.name} = import ./hosts/home.nix;
+              extraSpecialArgs = {
+                inherit inputs;
+                meta = host;
+                user = user;
+              };
+            };
+          }
+        ];
+      };
+    };
+  in {
+    nixosConfigurations = builtins.listToAttrs (map forLinuxHosts hosts);
   };
 }
