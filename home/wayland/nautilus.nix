e4c9be3fda7079108ab039b20121e2a23b1a1abd
melurann <fruroa21@htl-kaindorf.at>
2026-01-28 08:31:27 +0100

    chore(nautilus): deadnix run

--- home/wayland/nautilus.nix
+++ home/wayland/nautilus.nix
@@ -1,7 +1,7 @@
 {gst_all_1, ...}: {
   nixpkgs.overlays = [
-    (self: super: {
-      gnome = super.gnome.overrideScope' (gself: gsuper: {
+    (_self: super: {
+      gnome = super.gnome.overrideScope' (_gself: gsuper: {
         nautilus = gsuper.nautilus.overrideAttrs (nsuper: {
           buildInputs =
             nsuper.buildInputs
