2aa6706c78b0dee05efd2155d1bc46fd83a81f65
melurann <fruroa21@htl-kaindorf.at>
2026-02-12 00:19:24 +0100

    chore(home): remove duplicate code

--- hosts/home.nix
+++ hosts/home.nix
@@ -3,16 +3,9 @@
     ../home
   ];
 
-  home.username = user.name;
-  home.homeDirectory = user.homeDir;
-  home.stateVersion = "25.05";
-
-  programs.git.enable = true;
-
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      san = "echo sanity check o.O";
-    };
+  home = {
+    username = user.name;
+    homeDirectory = user.homeDir;
+    stateVersion = "25.05";
   };
 }
