4ba4e24190cde1f010c57ad1409df10ebddfee8f
melurann <fruroa21@htl-kaindorf.at>
2026-04-06 14:30:37 +0200

    feat(nvim-treesitter): update to upstream and remove lazy loading

--- home/dev/editors/nvim/nvim/lua/plugins/treesitter.lua
+++ home/dev/editors/nvim/nvim/lua/plugins/treesitter.lua
@@ -1,99 +1,72 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			require("nvim-treesitter.install").update({ with_sync = true })
-		end,
-		event = { "BufEnter" },
-		dependencies = {
-			-- Additional text objects for treesitter
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		config = function()
-			---@diagnostic disable: missing-fields
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"bash",
-					"c",
-					"css",
-					"gleam",
-					"graphql",
-					"html",
-					"javascript",
-					"json",
-					"lua",
-					"markdown",
-					"ocaml",
-					"ocaml_interface",
-					"prisma",
-					"tsx",
-					"typescript",
-					"vim",
-					-- "yaml", This is currently borked see: https://github.com/ikatyang/tree-sitter-yaml/issues/53
-				},
-				sync_install = false,
-				highlight = {
-					enable = true,
-				},
-				indent = {
-					enable = true,
-					disable = { "ocaml", "ocaml_interface" },
-				},
-				autopairs = {
-					enable = true,
-				},
-				autotag = {
-					enable = true,
-				},
-				--[[ context_commentstring = {
-					enable = true,
-					enable_autocmd = false,
-				}, ]]
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<c-space>",
-						node_incremental = "<c-space>",
-						scope_incremental = "<c-s>",
-						node_decremental = "<c-backspace>",
-					},
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]m"] = "@function.outer",
-							["]]"] = "@class.outer",
-						},
-						goto_next_end = {
-							["]M"] = "@function.outer",
-							["]["] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[m"] = "@function.outer",
-							["[["] = "@class.outer",
-						},
-						goto_previous_end = {
-							["[M"] = "@function.outer",
-							["[]"] = "@class.outer",
-						},
-					},
-				},
-			})
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = false,
+        build = ":TSUpdate",
+        dependencies = {
+            {
+                -- Additional text objects for treesitter
+                "nvim-treesitter/nvim-treesitter-textobjects",
+                branch = "main",
+
+            },
+        },
+        config = function()
+            ---@diagnostic disable: missing-fields
+            require("nvim-treesitter").setup({
+                ensure_installed = {
+                    "c",
+                    "rust",
+                    "zig",
+                    "go",
+                    "cpp",
+                    "bash",
+                    "css",
+                    "html",
+                    "json",
+                    "lua",
+                    "markdown",
+                    "typescript",
+                    -- "yaml", This is currently borked see: https://github.com/ikatyang/tree-sitter-yaml/issues/53
+                },
+            })
+
+            require("nvim-treesitter-textobjects").setup({
+                select = {
+                    enable = true,
+                    lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true, -- whether to set jumps in the jumplist
+                    goto_next_start = {
+                        ["]m"] = "@function.outer",
+                        ["]]"] = "@class.outer",
+                    },
+                    goto_next_end = {
+                        ["]M"] = "@function.outer",
+                        ["]["] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@function.outer",
+                        ["[["] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        ["[M"] = "@function.outer",
+                        ["[]"] = "@class.outer",
+                    },
+                },
+            })
+        end,
+    },
 }
